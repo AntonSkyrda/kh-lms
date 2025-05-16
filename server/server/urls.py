8a9e81fceac65889a8f04b9a4f91d623ac173f4c
AntonSkyrda <mr.skyrda@gmail.com>
2025-05-16 21:26:27 +0300

    add default authentication with jwt, add login and refresh token urls

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api_v1/courses/", include("courses.urls")),
     path("api_v1/groups/", include("groups.urls")),
     path("api_v1/users/", include("users.urls")),
+    path("api_v1/auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api_v1/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
