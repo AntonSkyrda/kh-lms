919824d619e8708c1b36f225143d7820bed392cb
AntonSkyrda <mr.skyrda@gmail.com>
2025-05-16 13:11:08 +0300

    create teachers list view and students list view

--- server/users/views.py
+++ server/users/views.py
@@ -1,7 +1,11 @@
-from rest_framework.generics import RetrieveAPIView
+from django.contrib.auth import get_user_model
+from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
-from .serializers import UserMeSerializer
+from .serializers import UserMeSerializer, UserListSerializer
+
+
+User = get_user_model()
 
 
 class UserMeView(RetrieveAPIView):
@@ -10,3 +14,15 @@ class UserMeView(RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class TeacherListView(ListAPIView):
+    queryset = User.objects.filter(is_teacher=True)
+    serializer_class = UserListSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class StudentListView(ListAPIView):
+    queryset = User.objects.filter(is_student=True)
+    serializer_class = UserListSerializer
+    permission_classes = [IsAuthenticated]
