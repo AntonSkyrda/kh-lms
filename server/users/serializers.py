c15d4ed7e3f2f50f814ed72cb0e59cb9db068bb5
AntonSkyrda <mr.skyrda@gmail.com>
2025-05-20 15:39:55 +0300

    create student serializer

--- server/users/serializers.py
+++ server/users/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from .models import Student
+
 
 User = get_user_model()
 
@@ -35,7 +37,11 @@ class UserListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("id", "email", "full_name")
+        fields = (
+            "id",
+            "email",
+            "full_name",
+        )
 
     def get_full_name(self, obj):
         return f"{obj.last_name} {obj.first_name} {obj.father_name}"
@@ -62,3 +68,19 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class StudentUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(source="user.email")
+    full_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Student
+        fields = (
+            "id",
+            "email",
+            "full_name",
+        )
+
+    def get_full_name(self, obj):
+        return f"{obj.user.last_name} {obj.user.first_name} {obj.user.father_name}"
