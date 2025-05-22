e595aa96d212047613361199a8caf5adb15b03c8
AntonSkyrda <mr.skyrda@gmail.com>
2025-05-23 02:06:49 +0300

    create homework and homework submissin model

--- server/homeworks/models.py
+++ server/homeworks/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Homework(models.Model):
+    lessons = models.ForeignKey(
+        "lessons.Lesson",
+        on_delete=models.CASCADE,
+        related_name="homeworks",
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    due_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.title} ({self.lessons})"
+
+
+class HomeworkSubmission(models.Model):
+    homework = models.ForeignKey(
+        "homeworks.Homework",
+        on_delete=models.CASCADE,
+        related_name="submissions",
+    )
+    student = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={"is_student": True},
+        related_name="homework_submissions",
+    )
+    answer = models.TextField()
+    submission_at = models.DateField(auto_now_add=True)
+    grade = models.PositiveSmallIntegerField(null=True, blank=True)
+    feedback = models.TextField(blank=True)
+
+    class Meta:
+        unique_together = (
+            "homework",
+            "student",
+        )
+
+    def __str__(self):
+        return f"{self.student.email} - {self.homework.title}"
