8ebc9a57008c1873fd67307fa54cd52f71d60b88
AntonSkyrda <mr.skyrda@gmail.com>
2025-05-25 15:11:33 +0300

    add homework submissions endpoint to urls

--- server/homeworks/urls.py
+++ server/homeworks/urls.py
@@ -2,10 +2,11 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import HomeworkViewSet
+from .views import HomeworkViewSet, HomeworkSubmissionViewSet
 
 router = DefaultRouter()
 router.register("", HomeworkViewSet, basename="homeworks")
+router.register("submissions", HomeworkSubmissionViewSet, basename="submissions")
 urlpatterns = [
     path("", include(router.urls)),
 ]
